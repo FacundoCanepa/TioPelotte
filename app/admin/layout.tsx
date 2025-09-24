fb85103c7ef492981158a5921a1b0ab1da85ec5f
facundocanepa40 <facundocanepa40@gmail.com>
2025-09-24 00:05:57 +0000

    codex

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,15 +1,19 @@
+
 import AdminGuard from "@/components/guards/AdminGuard";
 import { Sidebar } from "@/components/layout/sidebar";
+import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AdminGuard>
-      <div className="min-h-screen text-[#5A3E1B] p-6 flex">
-        <Sidebar />
-        <main className="flex-1 p-6">
-          {children}
-        </main>
-      </div>
-    </AdminGuard>
+    <ReactQueryProvider>
+      <AdminGuard>
+        <div className="min-h-screen text-[#5A3E1B] p-6 flex">
+          <Sidebar />
+          <main className="flex-1 p-6">
+            {children}
+          </main>
+        </div>
+      </AdminGuard>
+    </ReactQueryProvider>
   );
 }
