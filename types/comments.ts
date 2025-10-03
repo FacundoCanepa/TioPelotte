9b7cd21b8be787124586ff398ef72f6c143ceecc
facundocanepa40 <facundocanepa40@gmail.com>
2025-10-03 18:50:47 +0000

    ad

--- types/comments.ts
+++ types/comments.ts
@@ -1,24 +1,24 @@
 export const googleReviews = [
   {
-    name: "María López",
+    name: "Andres Garcia",
     rating: 5,
     date: "hace 2 semanas",
-    comment: "¡Las pastas son increíbles! Siempre frescas y deliciosas.",
-    avatar: "/unnamed_180aa5475f.png"
+    comment: "Excelente pastas frescas para comer con la familia. Su dueño y la atención es impecable. Las mejores del oeste platense sin duda!!!",
+    avatar: "/unnamed_c228c976ec.png"
   },
   {
-    name: "Juan Pérez",
+    name: "Norma Angelani",
     rating: 4,
     date: "hace 1 mes",
-    comment: "Buena atención y productos de calidad. Volveré pronto.",
-    avatar: "/unnamed_180aa5475f.png"
+    comment: "Muy buena atención  buena calidad de los productos muy buena gente.",
+    avatar: "/unnamed_1_94c9fdc8d0.png"
   },
   {
     name: "Lucía Fernández",
     rating: 5,
     date: "hace 3 días",
-    comment: "Los ravioles de verdura son los mejores que he probado.",
-    avatar: "/unnamed_180aa5475f.png"
+    comment: "Pastas frescas y muy ricas, los ravioles de calabaza y queso exquisitos",
+    avatar: "/unnamed_2_98ad0b9d24.png"
   },
   
 ]
